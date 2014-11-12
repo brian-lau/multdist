c89788e3152518cf150408bdb9d828849ab83ca5
Brian Lau <brian.lau@upmc.fr>
2014-11-12 11:15:33 +0100

    ENH: add Hotelling T2 test

---
+++ hotell2.m
@@ -0,0 +1,73 @@
+% HOTELL2                     Hotelling's T-Squared test for two multivariate samples 
+% 
+%     [pval,T2] = hotell2(x,y)
+%
+%     Hotelling's T-Squared test for comparing d-dimensional data from two 
+%     independent samples.
+%
+%     INPUTS
+%     x    - [n1 x d] matrix
+%     y    - [n2 x d] matrix
+%
+%     OUTPUTS
+%     pval - p-value by permutation
+%     T2   - minimum energy statistic
+%
+%     SEE ALSO
+%     kstest2d, minentest
+
+%     $ Copyright (C) 2014 Brian Lau http://www.subcortex.net/ $
+%     The full license and most recent version of the code can be found on GitHub:
+%     https://github.com/brian-lau/multdist
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function [pval,T2] = hotell2(x,y)
+
+[nx,px] = size(x);
+[ny,py] = size(y);
+
+if px ~= py
+   error('# of columns in X and Y must match');
+else
+   p = px;
+end
+
+mux = mean(x);
+muy = mean(y);
+
+Sx = cov(x);
+Sy = cov(y);
+Sp = ((nx-1)*Sx + (ny-1)*Sy) / (nx+ny-2);
+
+d = mux - muy;
+T2 = d * inv(Sp*(1/nx + 1/ny)) * d';
+F = T2 * (nx+ny-p-1) / (p*(nx+ny-2));
+pval = 1 - fcdf(F,p,nx+ny-p-1);
+
+if nargout == 0
+   fprintf('-------------------------------\n');
+   fprintf('  nx = %g\n',nx);
+   fprintf('  ny = %g\n',ny);
+   fprintf('  mean(x) = ');
+   fprintf('%1.3f, ',mux);
+   fprintf('\n');
+   fprintf('  mean(y) = ');
+   fprintf('%1.3f, ',muy);
+   fprintf('\n');
+   fprintf('  T2 = %5.3f\n',T2);
+   fprintf('  F(%g,%g) = %5.3f\n',p,nx+ny-p-1,F);
+   fprintf('  p = %5.5f\n',pval);
+   fprintf('-------------------------------\n');
+end
